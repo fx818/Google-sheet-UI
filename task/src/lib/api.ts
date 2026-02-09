ed077835e3b31a487075b40512dd2e02584bb6bd
Anurag Upadhyay <anuragfx818@gmail.com>
2026-02-09 13:17:59 +0530

    added envc

--- task/src/lib/api.ts
+++ task/src/lib/api.ts
@@ -1,4 +1,8 @@
-const BACKEND_URL = 'http://localhost:8080';
+// const BACKEND_URL =  'http://localhost:8080';
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080';
+
+console.log('Using backend URL:', BACKEND_URL);
 
 export interface TaskItem {
   task: string;
